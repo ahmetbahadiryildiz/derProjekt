bf2d953f801fef8d71cd204bfc9c3d1d21452c3f
Ahmet Bahadır YILDIZ <yildiz.bahadir@metu.edu.tr>
2024-01-31 00:21:08 +0300

    backup 31.01.24

--- app/src/main/java/com/greemlock/derprojekt/Adapter/BlogCardAdapter.java
+++ app/src/main/java/com/greemlock/derprojekt/Adapter/BlogCardAdapter.java
@@ -1,5 +1,10 @@
 package com.greemlock.derprojekt.Adapter;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,21 +12,81 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.greemlock.derprojekt.Objects.Blog;
 import com.greemlock.derprojekt.R;
 
-public class BlogCardAdapter extends RecyclerView.Adapter<BlogCardAdapter.BlogCardHolder2> {
+import java.io.File;
+import java.util.List;
+
+public class BlogCardAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+    private List<Blog> blogList;
+
+    public BlogCardAdapter(Context context, List<Blog> blogList){
+        this.context = context;
+        this.blogList = blogList;
+    }
+
     class BlogCardHolder2 extends RecyclerView.ViewHolder{
         private TextView textViewBlogTitle, textViewBlogSubtitle;
         private ImageView imageViewBlog;
+        public ConstraintLayout constraintLayoutBlog;
 
         BlogCardHolder2(View itemView){
             super(itemView);
             textViewBlogTitle = itemView.findViewById(R.id.textViewBlogTitle);
             textViewBlogSubtitle = itemView.findViewById(R.id.textViewBlogSubtite);
             imageViewBlog = itemView.findViewById(R.id.imageViewBlog);
+            constraintLayoutBlog = itemView.findViewById(R.id.constraintLayoutBlog);
         }
 
+    }
+
+    @NonNull
+    @Override
+    public BlogCardHolder2 onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.blog_post,viewGroup,false);
+        return new BlogCardHolder2(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        Blog blog = blogList.get(position);
+
+        BlogCardHolder2 blogCardHolder2= (BlogCardHolder2) holder;
+        blogCardHolder2.textViewBlogTitle.setText(blog.getBlogTitle());
+        blogCardHolder2.textViewBlogSubtitle.setText(blog.getBlogSubtitle());
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        StorageReference blogPhoto = storageReference.child("blogPhotos/"+ blog.getBlogKey());
+
+        try{
+            File file = File.createTempFile("images","jpg");
+            blogPhoto.getFile(file).addOnSuccessListener(taskSnapshot -> {
+                String filePath = file.getPath();
+                Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                blogCardHolder2.imageViewBlog.setImageBitmap(bitmap);
+            });
+        }
+        catch (Exception e){
+            Log.e("error",e.getLocalizedMessage());
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 }
--- app/src/main/java/com/greemlock/derprojekt/Fragments/BlogFragment.java
+++ app/src/main/java/com/greemlock/derprojekt/Fragments/BlogFragment.java
@@ -2,24 +2,73 @@ package com.greemlock.derprojekt.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.greemlock.derprojekt.Adapter.BlogCardAdapter;
+import com.greemlock.derprojekt.Objects.Blog;
 import com.greemlock.derprojekt.R;
+
+import java.util.ArrayList;
 
 public class BlogFragment extends Fragment {
 
-    public BlogFragment() {
-        // Required empty public constructor
-    }
+    private RecyclerView recyclerView;
+    private BlogCardAdapter blogCardAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ArrayList<Blog> blogList = new ArrayList<>();
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("blogs");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                blogList.clear();
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    Blog blog = dataSnapshot.getValue(Blog.class);
+                    blogList.add(blog);
+                }
+                blogCardAdapter = new BlogCardAdapter(getActivity(),blogList);
+                recyclerView.setAdapter(blogCardAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        recyclerView = getActivity().findViewById(R.id.recyclerViewBlog);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        blogCardAdapter = new BlogCardAdapter(getActivity(),blogList);
+        recyclerView.setAdapter(blogCardAdapter);
+
+
     }
 }
